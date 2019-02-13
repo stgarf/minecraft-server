bc9ee7506b6610948f70fde1073261f47b5f8e04
Steve Garf <stgarf@gmail.com>
2019-02-13 02:04:45 -0800

    Added missing binaries, fixed downlading, and handle eula.
    * Added jq, curl
    * Fix server.jar downloading
    * Properly handle license agreement.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM openjdk:8-jdk-alpine3.9
 USER root
 
-RUN apk add --update bash && \
+RUN apk add --no-cache --update bash curl jq && \
     addgroup -g 1000 minecraft && \
     adduser -h /home/minecraft -G minecraft -D -u 1000 minecraft
 
@@ -18,4 +18,4 @@ COPY docker-entrypoint.sh /
 ENTRYPOINT ["/docker-entrypoint.sh"]
 USER minecraft
 WORKDIR /server-data
-CMD ["java", "-Xmx1024M", "-Xms1024M", "-jar", "server.jar", "nogui"]
+CMD ["java", "-Xmx1024M", "-Xms1024M", "-jar", "/home/minecraft/server.jar", "nogui"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,14 +3,21 @@ echo "Initializing Minecraft server version $VERSION"
 
 # Fetch given server version
 echo "Fetching server.jar version: $VERSION"
-curl -s https://launchermeta.mojang.com/mc/game/version_manifest.json | \
-	jq --arg VERSION "$VERSION" -r '.versions[] | select(.id == $VERSION) \
-	| {url} | .url' | xargs curl -s | jq -r '.downloads.server.url' | \
-	xargs curl -so $USER/server.jar
+pushd $HOME && \
+	curl -sL https://launchermeta.mojang.com/mc/game/version_manifest.json | \
+	jq --arg VERSION "$VERSION" -r \
+	'.versions[] | select(.id == $VERSION) | {url} | .url' | xargs curl -sL | \
+	jq -r '.downloads.server.url' | xargs curl -sLO && \
+	popd
 
 # Handle license
-#echo "eula=$EULA" >> /server-data/eula.txt
-#if [ $? != 0 ]; then echo "ERROR: Persistent Volume Claim is unavailable!"; exit 2; fi
+if [ -f /server-data/eula.txt ]; then
+	sed -i 's/eula=false/eula=true/g' /server-data/eula.txt
+else
+	echo "eula=$EULA" >> /server-data/eula.txt
+fi
+
+if [ $? != 0 ]; then echo "ERROR: Persistent Volume Claim is unavailable!"; exit 2; fi
 
 # Run server.jar with java command.
 exec "$@"
